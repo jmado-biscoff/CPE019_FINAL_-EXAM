e542c22f2984d0460a4901ed5af6e33e6ca78553
jmado-biscoff <qjmadeomampo@tip.edu.ph>
2025-05-24 01:03:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from PIL import Image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-model = load_model('NasNet_model.keras',compile = False)
+model = load_model('NasNet_model.h5')
 
 class_names = ['cloudy', 'rain', 'shine', 'sunrise']
 
