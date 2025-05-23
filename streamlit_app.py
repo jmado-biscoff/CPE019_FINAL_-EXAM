fc261432ca282259a4d206c4e7404c020525ebc9
jmado-biscoff <qjmadeomampo@tip.edu.ph>
2025-05-24 01:07:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,25 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications import NASNetMobile
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.optimizers import Adam
 
-model = load_model('NasNet_model.h5')
+# Rebuild model architecture
+base_model = NASNetMobile(weights=None, include_top=False, input_shape=(150, 150, 3))
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dropout(0.3)(x)
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.3)(x)
+predictions = Dense(4, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+model.load_weights('nasnet_weights.h5')
+model.compile(optimizer=Adam(learning_rate=0.0001),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 class_names = ['cloudy', 'rain', 'shine', 'sunrise']
 
@@ -13,11 +28,11 @@ st.title("Weather Image Classifier (NASNet)")
 uploaded_file = st.file_uploader("Upload a weather image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    img = Image.open(uploaded_file).resize((224, 224))
+    img = Image.open(uploaded_file).resize((150, 150))
     st.image(img, caption='Uploaded Image', use_container_width=True)
 
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
     prediction = model.predict(img_array)
     class_idx = np.argmax(prediction)
