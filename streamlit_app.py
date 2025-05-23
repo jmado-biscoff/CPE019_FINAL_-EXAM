e74891c1b0aa78cb0c2069ae6691997352b4759c
jmado-biscoff <qjmadeomampo@tip.edu.ph>
2025-05-23 22:09:55 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+model = load_model('NasNet_model.h5')
+
+class_names = ['cloudy', 'rain', 'shine', 'sunrise']
+
+st.title("Weather Image Classifier (NASNet)")
+
+uploaded_file = st.file_uploader("Upload a weather image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file).resize((224, 224))
+    st.image(img, caption='Uploaded Image', use_container_width=True)
+
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0) / 255.0
+
+    prediction = model.predict(img_array)
+    class_idx = np.argmax(prediction)
+
+    st.write(f"**Prediction:** {class_names[class_idx]}")
